5702a1b55bb841d48973af788c1cd3edbe62a4c0
narustudio <slackjawed12@gmail.com>
2023-04-10 23:38:29 +0900

    feat : Spring REST Docs 의존성 추가 및 예약 등록 문서화 시도

---
+++ src/test/java/com/example/chillisauce/docs/ApiDocumentUtil.java
@@ -0,0 +1,16 @@
+package com.example.chillisauce.docs;
+
+import org.springframework.restdocs.operation.preprocess.OperationRequestPreprocessor;
+import org.springframework.restdocs.operation.preprocess.OperationResponsePreprocessor;
+
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.*;
+
+public interface ApiDocumentUtil {
+    static OperationRequestPreprocessor getDocumentRequest() {
+        return preprocessRequest(prettyPrint());
+    }
+
+    static OperationResponsePreprocessor getDocumentResponse(){
+        return preprocessResponse(prettyPrint());
+    }
+}
