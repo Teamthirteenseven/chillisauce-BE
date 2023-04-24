318fc61a78dc5de28d909eff998bfb572a802a34
sangggyu <124053404+sangggyu@users.noreply.github.com>
2023-04-24 16:25:26 +0900

    Feature redis (#163) (#164)
    * Feature : 예약 서비스 조회 테스트 작성 (#143) (#144)
    
    * test : 예약 전체 조회 테스트 작성
    
    * refactor : 예약 타임테이블 관리 객체 생성
    
    * refactor : 예약 타임테이블 관리 객체 생성
    
    * test : 당일 회의 예약 조회 테스트 작성
    
    * docs : Spring REST docs 내용 반영
    
    * Test : AdminService 테스트코드 작성
    
    * fix : 토큰 만료시간 원상복구
    
    * ci : gradle-build-action v2로 변경 (#149)
    
    * ci : gradle cache 추가 (#150)
    
    * feat : redis 구현
    
    * fix : redis 코드수정
    
    ---------
    
    Co-authored-by: narustudio <77224652+slackjawed12@users.noreply.github.com>
    Co-authored-by: hyukjin jang <chaos2061@naver.com>

--- src/test/java/com/example/chillisauce/ChillisauceApplicationTests.java
+++ src/test/java/com/example/chillisauce/ChillisauceApplicationTests.java
@@ -2,8 +2,10 @@ package com.example.chillisauce;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootTest
+@EnableCaching
 class ChillisauceApplicationTests {
 
     @Test
