25716372a2966ada7f976e24005b45baf82ee7bf
narustudio <slackjawed12@gmail.com>
2023-04-24 17:42:36 +0900

    fix : list 라이브러리 추가

--- src/main/java/com/example/chillisauce/spaces/service/SpaceService.java
+++ src/main/java/com/example/chillisauce/spaces/service/SpaceService.java
@@ -21,6 +21,8 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
