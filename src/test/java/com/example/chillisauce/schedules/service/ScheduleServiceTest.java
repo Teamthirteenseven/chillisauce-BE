55e933465295307810d318811c15c31b6d13e3d0
narustudio <77224652+slackjawed12@users.noreply.github.com>
2023-04-26 22:55:57 +0900

    test : 개인 스케줄 도메인 테스트 작성 (#205)
    * feature : 유저 검색 기능 1차 구현 (#201)
    
    * fix : space 선택조회 username 추가 (#199)
    
    * feat : 연관관계 수정 및 Loaction 엔티티 추가
    
    * feat : location구현
    
    * feat : redis
    
    * feat : location 구현
    
    * fix: redis key값 수정
    
    * fix: redis time 1분 수정
    
    * fix: locationId수정
    
    * fix: locationId수정
    
    * fix: redis주석처리
    
    * fix: redis주석
    
    * fix: multibox API 수정
    
    * feat: boxResponseDto 유저네임
    
    * feat: boxResponseDto 유저네임
    
    * fix: locationService 수정
    
    * fix: space 선택조회 username 수정
    
    * feat : 유저 검색 기능 1차 구현
    
    * feat : 유저 검색 기능 구현
    
    ---------
    
    Co-authored-by: sangggyu <124053404+sangggyu@users.noreply.github.com>
    
    * fix: multibox userlist추가 (#203)
    
    * feat : 연관관계 수정 및 Loaction 엔티티 추가
    
    * feat : location구현
    
    * feat : redis
    
    * feat : location 구현
    
    * fix: redis key값 수정
    
    * fix: redis time 1분 수정
    
    * fix: locationId수정
    
    * fix: locationId수정
    
    * fix: redis주석처리
    
    * fix: redis주석
    
    * fix: multibox API 수정
    
    * feat: boxResponseDto 유저네임
    
    * feat: boxResponseDto 유저네임
    
    * fix: locationService 수정
    
    * fix: space 선택조회 username 수정
    
    * fix: multibox userlist 추가 (#204)
    
    * feat : 연관관계 수정 및 Loaction 엔티티 추가
    
    * feat : location구현
    
    * feat : redis
    
    * feat : location 구현
    
    * fix: redis key값 수정
    
    * fix: redis time 1분 수정
    
    * fix: locationId수정
    
    * fix: locationId수정
    
    * fix: redis주석처리
    
    * fix: redis주석
    
    * fix: multibox API 수정
    
    * feat: boxResponseDto 유저네임
    
    * feat: boxResponseDto 유저네임
    
    * fix: locationService 수정
    
    * fix: space 선택조회 username 수정
    
    * fix: multibox userlist추가
    
    * fix : 충돌 수정과정에서 삭제 누락된 부분 해결
    
    * test : 개인 스케줄 조회 및 등록 테스트
    
    * test : 스케줄 도메인 수정, 삭제 테스트 작성
    
    ---------
    
    Co-authored-by: sangggyu <124053404+sangggyu@users.noreply.github.com>

--- src/test/java/com/example/chillisauce/schedules/service/ScheduleServiceTest.java
+++ src/test/java/com/example/chillisauce/schedules/service/ScheduleServiceTest.java
@@ -1,85 +1,294 @@
 package com.example.chillisauce.schedules.service;
 
+import com.example.chillisauce.reservations.exception.ReservationException;
+import com.example.chillisauce.reservations.vo.ReservationTimetable;
+import com.example.chillisauce.schedules.dto.*;
+import com.example.chillisauce.schedules.entity.Schedule;
+import com.example.chillisauce.schedules.exception.ScheduleException;
+import com.example.chillisauce.schedules.repository.ScheduleRepository;
+import com.example.chillisauce.security.UserDetailsImpl;
+import com.example.chillisauce.users.entity.User;
+import com.example.chillisauce.users.entity.UserRoleEnum;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 @DisplayName("ScheduleService 클래스")
 class ScheduleServiceTest {
 
+    @InjectMocks
+    ScheduleService scheduleService;
+
+    @Mock
+    ScheduleRepository scheduleRepository;
+
     @Nested
     @DisplayName("getAllSchedules 메서드는")
     class GetAllSchedulesTestCase {
-        @Test
-        void 회원의_전체_스케줄을_조회한다(){
+        // given
+        User user = User.builder()
+                .id(1L)
+                .email("test@test.com")
+                .username("testUser")
+                .role(UserRoleEnum.USER)
+                .build();
+        UserDetailsImpl userDetails = new UserDetailsImpl(user, user.getUsername());
+        Schedule scheduleOne = Schedule.builder()
+                .id(1L)
+                .startTime(LocalDateTime.of(2023, 4, 26, 15, 0))
+                .endTime(LocalDateTime.of(2023, 4, 26, 16, 0))
+                .user(user)
+                .title("testScheduleOne")
+                .build();
 
+        Schedule scheduleTwo = Schedule.builder()
+                .id(2L)
+                .startTime(LocalDateTime.of(2023, 4, 26, 19, 0))
+                .endTime(LocalDateTime.of(2023, 4, 26, 20, 0))
+                .user(user)
+                .title("testScheduleTwo")
+                .build();
+        List<Schedule> schedules = List.of(scheduleOne, scheduleTwo);
+
+        @Test
+        void 회원의_전체_스케줄을_조회한다() {
+            // given
+            when(scheduleRepository.findAllByUserId(eq(user.getId()))).thenReturn(schedules);
+
+            // when
+            ScheduleListResponseDto result = scheduleService.getAllSchedules(userDetails);
+
+            // then
+            assertThat(result.getScList().size()).isEqualTo(2);
+            assertThat(result.getScList()).extracting("scId", Long.class)
+                    .contains(1L, 2L);
         }
     }
 
     @Nested
     @DisplayName("getDaySchedules 메서드는")
-    class GetDaySchedulesTestCase{
-        @Test
-        void 당일_스케줄을_조회한다(){
+    class GetDaySchedulesTestCase {
+        // given
+        User user = User.builder()
+                .id(1L)
+                .email("test@test.com")
+                .username("testUser")
+                .role(UserRoleEnum.USER)
+                .build();
+        UserDetailsImpl userDetails = new UserDetailsImpl(user, user.getUsername());
+        LocalDate selDate = LocalDate.of(2023, 4, 26);
+        Schedule scheduleOne = Schedule.builder()
+                .id(1L)
+                .startTime(LocalDateTime.of(2023, 4, 26, 15, 0))
+                .endTime(LocalDateTime.of(2023, 4, 26, 15, 59))
+                .user(user)
+                .title("testScheduleOne")
+                .build();
 
+        Schedule scheduleTwo = Schedule.builder()
+                .id(2L)
+                .startTime(LocalDateTime.of(2023, 4, 26, 19, 0))
+                .endTime(LocalDateTime.of(2023, 4, 26, 19, 59))
+                .user(user)
+                .title("testScheduleTwo")
+                .build();
+        List<Schedule> schedules = List.of(scheduleOne, scheduleTwo);
+
+        @Test
+        void 당일_스케줄을_조회한다() {
+            // given
+            when(scheduleRepository.findAllByUserIdAndStartTimeBetween(eq(1L), any(), any())).thenReturn(schedules);
+
+            // when
+            ScheduleTimetableResponseDto result = scheduleService.getDaySchedules(selDate, userDetails);
+
+            // then
+            assertThat(result.getTimeList())
+                    .isNotEmpty()
+                    .hasSize(ReservationTimetable.CLOSE_HOUR - ReservationTimetable.OPEN_HOUR + 1)
+                    .filteredOn(x -> x.getIsCheckOut().equals(true))
+                    .hasSize(2);
         }
     }
 
     @Nested
     @DisplayName("addSchedule 메서드는")
-    class AddScheduleTestCase{
+    class AddScheduleTestCase {
+        // given
+        User user = User.builder()
+                .id(1L)
+                .email("test@test.com")
+                .username("testUser")
+                .role(UserRoleEnum.USER)
+                .build();
+        UserDetailsImpl userDetails = new UserDetailsImpl(user, user.getUsername());
+        ScheduleRequestDto requestOne = ScheduleRequestDto.builder()
+                .scComment("test schedule comment")
+                .scTitle("test schedule title")
+                .startList(List.of(new ScheduleTime(LocalDateTime.of(2023, 4, 26, 15, 0))))
+                .build();
+
+        Schedule scheduleOne = Schedule.builder()
+                .id(1L)
+                .startTime(LocalDateTime.of(2023, 4, 26, 15, 0))
+                .endTime(LocalDateTime.of(2023, 4, 26, 16, 0))
+                .user(user)
+                .title("testScheduleOne")
+                .build();
+
         @Test
-        void 스케줄을_등록한다(){
+        void 스케줄을_등록한다() {
+            // given
+            when(scheduleRepository.save(any())).thenReturn(scheduleOne);
 
-        }
+            // when
+            ScheduleResponseDto result = scheduleService.addSchedule(requestOne, userDetails);
 
-        @Test
-        void 시간이_겹치면_예외가_발생한다(){
-
+            // then
+            assertThat(result.getScStart()).isEqualTo(scheduleOne.getStartTime());
+            assertThat(result.getScEnd()).isEqualTo(scheduleOne.getEndTime());
         }
     }
 
     @Nested
     @DisplayName("editSchedule 메서드는")
-    class EditScheduleTestCase{
-        @Test
-        void 스케줄을_수정한다(){
+    class EditScheduleTestCase {
+        // given
+        User user = User.builder()
+                .id(1L)
+                .email("test@test.com")
+                .username("testUser")
+                .role(UserRoleEnum.USER)
+                .build();
+        UserDetailsImpl userDetails = new UserDetailsImpl(user, user.getUsername());
+        Long scheduleId = 1L;
+        ScheduleRequestDto request = ScheduleRequestDto.builder()
+                .scComment("test schedule comment")
+                .scTitle("test schedule title")
+                .startList(List.of(new ScheduleTime(LocalDateTime.of(2023, 4, 26, 15, 0))))
+                .build();
+        Schedule scheduleOne = Schedule.builder()
+                .id(scheduleId)
+                .comment("before schedule comment")
+                .title("before schedule title")
+                .user(user)
+                .startTime(LocalDateTime.of(2023, 4, 26, 17, 0))
+                .endTime(LocalDateTime.of(2023, 4, 26, 17, 59))
+                .build();
 
+        @Test
+        void 스케줄을_수정한다() {
+            // given
+            when(scheduleRepository.findById(eq(1L))).thenReturn(Optional.of(scheduleOne));
+
+            // when
+            ScheduleResponseDto result = scheduleService.editSchedule(scheduleId, request, userDetails);
+
+            // then
+            assertThat(result.getScStart()).isEqualTo(LocalDateTime.of(2023, 4, 26, 15, 0));
         }
 
         @Test
-        void 시간이_겹치면_예외가_발생한다(){
+        void 스케줄이_없으면_예외가_발생한다() {
+            // given
+            Long wrongId = 2L;
+            when(scheduleRepository.findById(eq(wrongId))).thenReturn(Optional.empty());
 
+            // when, then
+            assertThatThrownBy(()-> scheduleService.editSchedule(wrongId, request, userDetails))
+                    .isInstanceOf(ScheduleException.class).hasMessage("스케줄을 찾을 수 없습니다.");
         }
 
         @Test
-        void 권한이_없으면_예외가_발생한다(){
+        void 다른_유저가_요청하면_예외가_발생한다() {
+            // given
+            User another = User.builder()
+                    .id(2L)
+                    .email("test2@test.com")
+                    .username("anotherUser")
+                    .role(UserRoleEnum.USER)
+                    .build();
 
-        }
+            UserDetailsImpl anotherDetails = new UserDetailsImpl(another, another.getUsername());
 
-        @Test
-        void 스케줄이_없으면_예외가_발생한다(){
+            when(scheduleRepository.findById(eq(scheduleId))).thenReturn(Optional.of(scheduleOne));
 
+            // when, then
+            assertThatThrownBy(()-> scheduleService.editSchedule(scheduleId, request, anotherDetails))
+                    .isInstanceOf(ScheduleException.class).hasMessage("스케줄을 수정할 권한이 없는 유저입니다.");
         }
     }
 
     @Nested
     @DisplayName("deleteSchedule 메서드는")
-    class DeleteScheduleTestCase{
-        @Test
-        void 스케줄을_삭제한다(){
+    class DeleteScheduleTestCase {
+        // given
+        Long scheduleId = 1L;
+        User user = User.builder()
+                .id(1L)
+                .email("test@test.com")
+                .username("testUser")
+                .role(UserRoleEnum.USER)
+                .build();
+        UserDetailsImpl userDetails = new UserDetailsImpl(user, user.getUsername());
 
+        Schedule scheduleOne = Schedule.builder()
+                .id(scheduleId)
+                .comment("before schedule comment")
+                .title("before schedule title")
+                .user(user)
+                .startTime(LocalDateTime.of(2023, 4, 26, 17, 0))
+                .endTime(LocalDateTime.of(2023, 4, 26, 17, 59))
+                .build();
+        @Test
+        void 스케줄을_삭제한다() {
+            // given
+            when(scheduleRepository.findById(eq(scheduleId))).thenReturn(Optional.of(scheduleOne));
+
+            // when
+            String result = scheduleService.deleteSchedule(scheduleId, userDetails);
+
+            // then
+            assertThat(result).isEqualTo("success");
         }
-        @Test
-        void 권한이_없으면_예외가_발생한다(){
 
+        @Test
+        void 다른_유저가_요청하면_예외가_발생한다() {
+            // given
+            User another = User.builder()
+                    .id(2L)
+                    .email("test2@test.com")
+                    .username("anotherUser")
+                    .role(UserRoleEnum.USER)
+                    .build();
+
+            UserDetailsImpl anotherDetails = new UserDetailsImpl(another, another.getUsername());
+
+            when(scheduleRepository.findById(eq(scheduleId))).thenReturn(Optional.of(scheduleOne));
+
+            // when, then
+            assertThatThrownBy(()-> scheduleService.deleteSchedule(scheduleId, anotherDetails))
+                    .isInstanceOf(ScheduleException.class).hasMessage("스케줄을 삭제할 권한이 없는 유저입니다.");
         }
 
         @Test
-        void 스케줄이_없으면_예외가_발생한다(){
+        void 스케줄이_없으면_예외가_발생한다() {
 
         }
     }
