da8e4c6f55aa98a16ad07d71da3dccfd5e4a219b
narustudio <slackjawed12@gmail.com>
2023-04-27 20:07:09 +0900

    feat : 포트 스위치 스크립트 작성

--- scripts/switch.sh
+++ scripts/switch.sh
@@ -1,21 +1,33 @@
 #!/usr/bin/env bash
 
 PROJECT_ROOT="/home/ubuntu/app"
-JAR_FILE="$PROJECT_ROOT/chillisauce.jar"
 
-APP_LOG="$PROJECT_ROOT/application.log"
-ERROR_LOG="$PROJECT_ROOT/error.log"
 DEPLOY_LOG="$PROJECT_ROOT/deploy.log"
 
-TIME_NOW=$(date +%c)
+CURRENT_PORT=$(cat /home/ubuntu/service_url.inc | grep -Po '[0-9]+' | tail -1)
+TARGET_PORT=0
 
-# build 파일 복사
-echo "$TIME_NOW > $JAR_FILE 파일 복사" >> $DEPLOY_LOG
-cp $PROJECT_ROOT/build/libs/*.jar $JAR_FILE
+if [ $CURRENT_PORT -eq 8081 ]; then
+  TARGET_PORT=8082
+elif [ $CURRENT_PORT -eq 8082 ]; then
+  TARGET_PORT=8081
+else
+  echo "> No WAS is connected to nginx"
+  exit 1
+fi
 
-# jar 파일 실행
-echo "$TIME_NOW > $JAR_FILE 파일 실행" >> $DEPLOY_LOG
-nohup java -jar $JAR_FILE > $APP_LOG 2> $ERROR_LOG &
+# 프록시 포트번호 변경
+echo "set \$service_url http://127.0.0.1:${TARGET_PORT};" | tee /home/ubuntu/service_url.inc
+echo "> Now Nginx proxies to ${TARGET_PORT}." >> $DEPLOY_LOG
 
-CURRENT_PID=$(pgrep -f $JAR_FILE)
-echo "$TIME_NOW > 실행된 프로세스 아이디 $CURRENT_PID 입니다." >> $DEPLOY_LOG
+# PREV_PID : 이전 PORT 번호로 구동 중인 애플리케이션 pid
+PREV_PID=$(lsof -Fp -i TCP:${CURRENT_PORT} | grep -Po 'p[0-9]+' | grep -Po '[0-9]+')
+
+# PREV_PID 구동중이면 kill
+if [ ! -z $PREV_PID ]; then
+  echo "$TIME_NOW > 실행중인 $PREV_PID 애플리케이션 종료 " >> $DEPLOY_LOG
+  kill -15 $PREV_PID
+fi
+
+# nginx reload
+sudo service nginx reload
--- src/main/java/com/example/chillisauce/healthcheck/HealthCheckController.java
+++ src/main/java/com/example/chillisauce/healthcheck/HealthCheckController.java
@@ -1,2 +1,12 @@
-package com.example.chillisauce.healthcheck;public class HealthCheckController {
+package com.example.chillisauce.healthcheck;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthCheckController {
+    @GetMapping("/server/healthcheck")
+    public String healthCheck() {
+        return "8080 health check OK";
+    }
 }
