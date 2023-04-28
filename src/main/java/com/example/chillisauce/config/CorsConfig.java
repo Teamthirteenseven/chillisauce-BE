0c7700c69bccb9b3b57b8eface9d417b87cef576
narustudio <77224652+slackjawed12@users.noreply.github.com>
2023-04-29 00:42:44 +0900

    fix : cors 설정 (#259)

--- src/main/java/com/example/chillisauce/config/CorsConfig.java
+++ src/main/java/com/example/chillisauce/config/CorsConfig.java
@@ -10,7 +10,7 @@ public class CorsConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOriginPatterns("*")
-                .allowedMethods("*")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowCredentials(true)
                 .maxAge(3000);
     }
