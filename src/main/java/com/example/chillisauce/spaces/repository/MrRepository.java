ae5a8a207a85b98ee20e362d6430cf6b62b7b86c
sangggyu <124053404+sangggyu@users.noreply.github.com>
2023-05-02 17:03:50 +0900

    fix: spaceResponseDto 수정 (#286)

--- src/main/java/com/example/chillisauce/spaces/repository/MrRepository.java
+++ src/main/java/com/example/chillisauce/spaces/repository/MrRepository.java
@@ -3,7 +3,6 @@ package com.example.chillisauce.spaces.repository;
 import com.example.chillisauce.spaces.entity.Floor;
 import com.example.chillisauce.spaces.entity.Mr;
 import com.example.chillisauce.users.entity.Companies;
-import com.querydsl.jpa.impl.JPAQueryFactory;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
