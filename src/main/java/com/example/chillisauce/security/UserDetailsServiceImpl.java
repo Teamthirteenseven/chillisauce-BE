fbcb71b544d98ba9ed0cb69445fb78a50ae99513
Jang hyuk jin <99636399+hyukjin1210@users.noreply.github.com>
2023-05-02 18:27:22 +0900

    feat : 유저 인증 정보 캐싱 (#290) (#291)
    * fix: spaceResponseDto 수정 (#286)
    
    * fix: allSpacelist 반환값 수정 (#287)
    
    * refactor: Mrlist 리팩토링 (#288) (#289)
    
    * fix: spaceResponseDto 수정 (#286)
    
    * fix: allSpacelist 반환값 수정 (#287)
    
    * refactor: mrlist 수정
    
    * refactor: mrlist리팩토링
    
    * feature : 유저 인증 정보 캐싱 적용
    
    ---------
    
    Co-authored-by: sangggyu <124053404+sangggyu@users.noreply.github.com>

--- src/main/java/com/example/chillisauce/security/UserDetailsServiceImpl.java
+++ src/main/java/com/example/chillisauce/security/UserDetailsServiceImpl.java
@@ -2,8 +2,13 @@ package com.example.chillisauce.security;
 
 import com.example.chillisauce.users.entity.User;
 import com.example.chillisauce.users.repository.UserRepository;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -18,6 +23,9 @@ public class UserDetailsServiceImpl implements UserDetailsService {
 
     // jwt토큰 발급절차에서 subject의 내용이 달라질 경우 이 로직에서도 수정이 필요
     @Override
+    /* 테스트1. 유저 인증객체인 UserDetailsImpl을 캐싱한다. */
+    @Cacheable(cacheNames = {"UserDetails"}, key = "#email")
+    /* 테스트1. 유저 인증객체인 UserDetailsImpl을 캐싱한다. */
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
         User user = userRepository.findByEmail(email)
                 .orElseThrow(() -> new UsernameNotFoundException("사용자를 찾을 수 없습니다."));
