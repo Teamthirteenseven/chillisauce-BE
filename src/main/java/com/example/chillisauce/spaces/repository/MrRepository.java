6d093fd85dc5196eda0dac332ebbadbe43937688
sanggyu <limsanggyu@kakao.com>
2023-05-03 15:42:36 +0900

    refactor : 미팅룸 조회 쿼리 변경

--- src/main/java/com/example/chillisauce/spaces/repository/MrRepository.java
+++ src/main/java/com/example/chillisauce/spaces/repository/MrRepository.java
@@ -17,7 +17,7 @@ public interface MrRepository extends JpaRepository <Mr, Long>{
     @Query("SELECT m FROM Mr m WHERE m.space.floor = :floor")
     List<Mr> findByFloor(@Param("floor")Floor floor);
 
-    @Query("SELECT m FROM Mr m JOIN m.reservations r JOIN m.space s WHERE s.companies.id = :companiesId")
+    @Query("SELECT m FROM Mr m LEFT JOIN FETCH m.reservations r JOIN m.space s WHERE s.companies.id = :companiesId")
     List<Mr> findAllByCompaniesId (@Param("companiesId") Long companiesId);
 
 
