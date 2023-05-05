1179544eb264009ca1533be52ca192a6b64b41c2
narustudio <77224652+slackjawed12@users.noreply.github.com>
2023-05-06 01:55:05 +0900

    chore : UserInjectRequest 수정 (#317)

--- src/main/java/com/example/chillisauce/performance/dto/UserInjectRequest.java
+++ src/main/java/com/example/chillisauce/performance/dto/UserInjectRequest.java
@@ -1,12 +1,14 @@
 package com.example.chillisauce.performance.dto;
 
-import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 
-/**
- * @param count 생성할 유저 수
- */
 @Builder
-public record UserInjectRequest(String email, String password, String passwordCheck, String username, Integer count) {
+@Getter
+public class UserInjectRequest {
+    String email;
+    String password;
+    String passwordCheck;
+    String username;
+    Integer count;
+}
