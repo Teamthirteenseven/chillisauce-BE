cc241501c92098729864ca013dceac4117d8ba30
Jang hyuk jin <99636399+hyukjin1210@users.noreply.github.com>
2023-05-06 16:36:32 +0900

    test : 성능개선 테스트(예약) (#329)

--- src/main/java/com/example/chillisauce/performance/dto/ReservationInjectRequest.java
+++ src/main/java/com/example/chillisauce/performance/dto/ReservationInjectRequest.java
@@ -1,6 +1,19 @@
 package com.example.chillisauce.performance.dto;
 
+import com.example.chillisauce.reservations.dto.request.ReservationTime;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Builder
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
 public class ReservationInjectRequest {
     // 생성 예약 수
-
+    List<ReservationTime> startList;
+    Integer count;
 }
