449133dbaab0e0773d6a19bac7ef1eb4a4025a28
narustudio <77224652+slackjawed12@users.noreply.github.com>
2023-05-06 17:06:09 +0900

    test : 스케줄 도메인 테스트객체 주입 구현 (#330)

--- src/main/java/com/example/chillisauce/performance/dto/ScheduleInjectRequest.java
+++ src/main/java/com/example/chillisauce/performance/dto/ScheduleInjectRequest.java
@@ -1,6 +1,16 @@
 package com.example.chillisauce.performance.dto;
 
+import com.example.chillisauce.schedules.dto.ScheduleTime;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
 public class ScheduleInjectRequest {
     // 생성할 일정 수
-    Integer count;
+    Integer days;
 }
