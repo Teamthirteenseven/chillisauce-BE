eec9e6d796145c9c6f84a21b55f0bff0fb81919e
hyukjin jang <chaos2061@naver.com>
2023-05-08 14:56:07 +0900

    test : 유저검색 Querydsl 변경

--- src/main/java/com/example/chillisauce/users/repository/UserRepositorySupport.java
+++ src/main/java/com/example/chillisauce/users/repository/UserRepositorySupport.java
@@ -1,6 +1,7 @@
 package com.example.chillisauce.users.repository;
 
 import com.example.chillisauce.users.entity.User;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.Optional;
@@ -8,4 +9,5 @@ import java.util.Optional;
 public interface UserRepositorySupport {
     Optional<User> findByIdAndCompanies_CompanyName (Long id, String companyName);
     List<User> findAllByCompanies_CompanyName (String companyName);
+    List<User> findAllByUsernameContainingAndCompanies(String name, String companyName);    //검색
 }
