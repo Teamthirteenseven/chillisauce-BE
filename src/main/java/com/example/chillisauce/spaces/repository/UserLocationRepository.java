2612ba57ef5ab9a6ae222224114cd14bc3ebd7a0
narustudio <slackjawed12@gmail.com>
2023-05-09 23:04:27 +0900

    feat : 비관적 락 적용

--- src/main/java/com/example/chillisauce/spaces/repository/UserLocationRepository.java
+++ src/main/java/com/example/chillisauce/spaces/repository/UserLocationRepository.java
@@ -4,12 +4,15 @@ import com.example.chillisauce.spaces.entity.Location;
 import com.example.chillisauce.spaces.entity.UserLocation;
 import com.example.chillisauce.users.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
 
+import javax.persistence.LockModeType;
 import java.util.List;
 import java.util.Optional;
 
 public interface UserLocationRepository extends JpaRepository<UserLocation, Long> {
 
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
     Optional<UserLocation> findByUserId(Long id);
     Optional<UserLocation> findByLocationId(Long locationId);
 
