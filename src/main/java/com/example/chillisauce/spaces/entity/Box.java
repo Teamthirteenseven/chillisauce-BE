20ef33699955974b043450a82a7b2cde4c5eb958
narustudio <77224652+slackjawed12@users.noreply.github.com>
2023-05-09 21:47:55 +0900

    feat : 유저 박스 이동 로직 수정 (#364)

--- src/main/java/com/example/chillisauce/spaces/entity/Box.java
+++ src/main/java/com/example/chillisauce/spaces/entity/Box.java
@@ -46,7 +46,5 @@ public class Box extends Location {
 
 
     }
-
-
 }
 
--- src/main/java/com/example/chillisauce/spaces/repository/LocationRepository.java
+++ src/main/java/com/example/chillisauce/spaces/repository/LocationRepository.java
@@ -3,12 +3,21 @@ package com.example.chillisauce.spaces.repository;
 import com.example.chillisauce.spaces.entity.Location;
 import com.example.chillisauce.users.entity.Companies;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
 public interface LocationRepository extends JpaRepository<Location, Long>{
 
-    Optional<Location> findByIdAndSpaceCompanies(Long locationId, Companies companies);
+    @Query("select l from Location l " +
+            "left join Space s " +
+            "on l.space.id=s.id " +
+            "left join Companies c " +
+            "on s.companies.id = c.id " +
+            "where l.id= :locationId and c.companyName = :companyName")
+    Optional<Location> findByIdAndCompanyName(@Param("locationId") Long locationId,
+                                              @Param("companyName") String companyName);
 
 }
 
