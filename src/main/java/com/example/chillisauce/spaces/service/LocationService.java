40477772277a06cfee3741423bf0b2b07db25bc2
narustudio <77224652+slackjawed12@users.noreply.github.com>
2023-05-09 23:05:03 +0900

    feat : 비관적 락 적용 (#366)

--- src/main/java/com/example/chillisauce/spaces/service/LocationService.java
+++ src/main/java/com/example/chillisauce/spaces/service/LocationService.java
@@ -28,7 +28,7 @@ public class LocationService {
 
     @Transactional
     @CacheEvict(cacheNames = {"SpaceResponseDtoList", "FloorResponseDtoList"}, allEntries = true)
-    public synchronized LocationDto moveWithUser(String companyName, Long locationId, UserDetailsImpl details) {
+    public LocationDto moveWithUser(String companyName, Long locationId, UserDetailsImpl details) {
 
         User user = details.getUser();
 
