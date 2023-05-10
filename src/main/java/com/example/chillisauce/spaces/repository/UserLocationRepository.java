e18ca134633307ae9c17b830f0110e979286fcbf
narustudio <77224652+slackjawed12@users.noreply.github.com>
2023-05-10 19:09:17 +0900

    feat : Pessimistic write로 롤백 테스트 (#373)

--- src/main/java/com/example/chillisauce/spaces/repository/UserLocationRepository.java
+++ src/main/java/com/example/chillisauce/spaces/repository/UserLocationRepository.java
@@ -2,10 +2,13 @@ package com.example.chillisauce.spaces.repository;
 
 import com.example.chillisauce.spaces.entity.UserLocation;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
 
+import javax.persistence.LockModeType;
 import java.util.Optional;
 
 public interface UserLocationRepository extends JpaRepository<UserLocation, Long> {
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
     Optional<UserLocation> findByUserId(Long id);
     Optional<UserLocation> findByLocationId(Long locationId);
 
